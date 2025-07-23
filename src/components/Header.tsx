fa62abb44283e4bf8748da8285878ecd3603382e
ARNAUD DEVOGE <arnoo.54@hotmail.fr>
2025-07-23 15:27:57 +0200

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header
+    () {
+    return (
+        <header className="bg-red-500 text-white p-4">
+            <h1 className="text-2xl font-bold">Générateur de blague Carambar !</h1>
+        </header>
+    );
+}
+
+export default Header;
